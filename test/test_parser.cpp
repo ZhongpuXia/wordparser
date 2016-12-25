b1af531419adcbe4cfe02e3b5d84ed98ac7b13ad
x593084577 <xiazhongpu5@163.com>
2016-12-25 22:06:59 +0800

    initialize unit test

---
+++ test/test_parser.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "parser.h"
+
+
+
+Test(Parser, ParserInt) {
+}
+
+
+int main(int argc, char* argv[]) {
+	testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
+	return 0;
+}
