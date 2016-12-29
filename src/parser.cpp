80de5eaf475fe3d21e3ca9dcc92e6f7f654ced33
x593084577 <xiazhongpu5@163.com>
2016-12-29 23:25:02 +0800

    add string split

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,15 +3,63 @@
 
 namespace parser {
 
-void Parser::Parser(std::string columns_type) {
+void Parser::Parser(const std::string& column_types) {
+	_delimiter = '\t';
+	_types = split(column_types, _delimiter);
 }
 
-void Parser::parse_line() {
+void Parser::Parser(const std::string& column_types, std::string delimiter{
+	_delimiter = delimiter;
+	_types = split(column_types, delimiter);
+}
+
+void Parser::parse_line(const std::string& str) {
+    std::vector<std::string> contents = split(str, _delimiter);
+
 }
 
 
-template typename T;
-void Parser::parse(char* str, T& value){
+std::vector<std::string> Parser::split(const std::string str, const std::string delimiter) {
+	std::size_t pos = 0;
+	std::size_t found = 0;
+	std::vector<std::string> str_vector;
+	while (pos != std::string::npos) {
+	    found = str.find_first_of(delimiter);
+		str_vector.emplace_back(str.substr(pos, found - pos));
+		pos = found;
+	}
+
+	return str_vector;
 }
 
+template<class T>
+T get_type(std::size_t index) {
+	type_str = _types[index];
+	switch(type_str) {
+		case "int": {
+			int result = 0;
+			return result;
+		}
+		case "float": {
+			float result = 0.0;
+			return result;
+		}
+		case "double": {
+			double result = 0.0;
+			return result;
+		}
+		case "char*": {
+			char* result = '\0';
+			return result;
+		}
+		case "User": {
+			User result;
+			return result;
+		}
+		default: {
+			std::cout << "undefined type: [" << type_str <<"], the type is string at default" << std::endl;
+			std::string result;
+			return result;
+		}
+}
 } //end of parser
--- src/parser.h
+++ src/parser.h
@@ -10,11 +10,18 @@
 namespace parser {
 class Parser {
 public:
-	explicit void Parser(std::string columns_type);
+	explicit void Parser(const std::string& column_types);
+	explicit void Parser(const std::string& column_types, std::string delimiter);
 	parse_line();
 
 private:
     std::vector<std::string> _types;
+	std::string _delimiter;
+
+	std::vector<std::string> split(const std::string str, const std::string delimiter);
+
+	template<class T>
+	T get_type(std::size_t index);
 
 };
 } // end of parser
